2130860afefde2b67cdf364552a4017fbe1809da
Tomasz Węgliński <weglinski.tomasz@gmail.com>
2021-10-29 22:59:33 +0200

    JavaScript array data structure example;

---
+++ JavaScript/array.js
@@ -0,0 +1,10 @@
+/*
+The most basic of all data structures, an array stores data in memory for later use. Each array has a fixed number of cells decided on its creation, and each cell has a corresponding numeric index used to select its data. Whenever you’d like to use the array, all you need are the desired indices, and you can access any of the data within.
+*/
+
+const array = [1,2,3,4];
+const arrayOfObjects = [
+    { key: 'foo' },
+    { key: 'bar' },
+    { key: 'baz' }
+];
